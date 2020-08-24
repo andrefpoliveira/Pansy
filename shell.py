fe9c54a9683e421c2aa22f4352ac64f99f1c97c2
BuildTools <unconfigured@null.spigotmc.org>
2020-08-24 17:20:43 +0100

    Finish Shell Added

--- shell.py
+++ shell.py
@@ -2,6 +2,9 @@ from lib import pansy
 
 while True:
     text = input('Pancy > ')
+
+    if text == "q": break
+    
     result, error = pansy.run('<stdin>', text)
 
     if error: print(error.as_string())
