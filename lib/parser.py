a571406dc8bfc9eb2c075b2bcfb88e5008092630
André Oliveira <39243097+andrefpoliveira@users.noreply.github.com>
2020-08-31 21:22:26 +0100

    Fix: end keyword for

--- lib/parser.py
+++ lib/parser.py
@@ -559,10 +559,10 @@ class Parser:
 			body = res.register(self.statements())
 			if res.error: return res
 
-			if not self.current_tok.matches(token.T_KEYWORD, 'END'):
+			if not self.current_tok.matches(token.T_KEYWORD, 'end'):
 				return res.failure(errors.InvalidSyntaxError(
 					self.current_tok.pos_start, self.current_tok.pos_end,
-					f"Expected 'END'"
+					f"Expected 'end'"
 				))
 
 			res.register_advancement()
