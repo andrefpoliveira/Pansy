24f3f18f289db7907031fef89d19bfb10e212579
BuildTools <unconfigured@null.spigotmc.org>
2020-09-01 21:39:57 +0100

    Added: CI support

--- shell.py
+++ shell.py
@@ -1,17 +1,31 @@
 from lib.interpreter import run
+import sys
 
-while True:
-    text = input('Pansy> ')
-    if text == "exit": break
-    if text.strip() == '': continue
-    result, error = run('<stdin>', text)
-
-    if error:
-        print(error.as_string())
-    elif result:
-        """
-        if len(result.elements) == 1:
-            print(repr(result.elements[0]))
+def main(run_test=None):
+    keep_run = True
+    while keep_run:
+        if run_test != None:
+            keep_run = False
+            text = run_test
         else:
-            print(repr(result))
-        """
+            text = input('Pansy> ')
+
+        if text == "exit": break
+        if text.strip() == '': continue
+        result, error = run('<stdin>', text)
+
+        if error:
+            print(error.as_string())
+        elif result:
+            """
+            if len(result.elements) == 1:
+                print(repr(result.elements[0]))
+            else:
+                print(repr(result))
+            """
+
+if __name__ == '__main__':
+    if len(sys.argv) != 1:
+        main(sys.argv[1])
+    else:
+        main()
---
+++ testing.py
@@ -0,0 +1,36 @@
+import sys
+import os
+import filecmp
+import shell
+
+#f1 executed
+#f2 on git
+def compareFiles(file1, file2):
+    f1 = [line.replace('\n','') for line in open(file1).readlines()]
+    f2 = [line.replace('\n','') for line in open(file2).readlines()]
+
+    print(f1)
+    print(f2)
+
+    if len(f1) - 1 > len(f2):
+        raise Exception("Some test is wrong: " + file2) 
+
+    for i in range(len(f2)):
+        if f1[i] != f2[i]:
+            raise Exception("Some line failed: " + file2)
+
+orig_stdout = sys.stdout
+example_path = "examples/"
+
+test_folders = list(filter(lambda x: len(x.split(".")) == 1, os.listdir('examples')))
+
+for i in test_folders:
+    f = open('out.txt', 'w')
+    sys.stdout = f
+    shell.main(f'run("{example_path + i}/code.pansy")')
+    f.flush()
+    compareFiles("out.txt", f"{example_path + i}/output.txt")
+    f.close()
+    
+
+sys.stdout = orig_stdout
